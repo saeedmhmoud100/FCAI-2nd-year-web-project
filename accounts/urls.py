d0238d34793a3b3fd0f8223288fa2b5c92f81069
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-05-10 01:43:53 +0300

    create Register new user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import login_view
+from . import views
 
 urlpatterns = [
 
-    path('login/', login_view, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, get_user_model
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
+
+User = get_user_model()
 
 def login_view(request):
     if request.method == 'POST':
@@ -18,3 +19,23 @@ def login_view(request):
             return render(request, 'accounts/login.html', {'error': 'Invalid username or password'})
 
     return render(request, 'accounts/login.html')
+
+
+def signup_view(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        re_password2 = request.POST.get('re-password')
+        if password == re_password2:
+            u = User.objects.create(username=username, email=email, password=password)
+            if request.POST.get('is_admin') == 'true':
+                u.is_admin = True
+                u.is_staff = True
+            u.save()
+        else:
+            return render(request, 'accounts/sign_up.html', {'error': 'Password does not match'})
+
+
+    return render(request, 'accounts/sign_up.html')
