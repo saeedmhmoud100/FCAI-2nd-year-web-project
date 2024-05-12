829bd3e08de9bbd6203914818f63a0fd4b472bbc
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-05-12 22:11:03 +0300

    show add new reviews in book_details page

--- books/templatetags/custom_filters.py
+++ books/templatetags/custom_filters.py
@@ -3,10 +3,19 @@ from django import template
 register = template.Library()
 
 
-@register.filter( name='not' )
+@register.filter(name='not')
 def not_gate(value):
     return not value
 
-@register.filter( name='my_range')
+
+@register.filter(name='my_range')
 def my_range(value):
-    return range(1,value+1)
+    return range(1, value + 1)
+
+
+@register.filter(name='is_not_object_owner')
+def is_not_object_owner(request_user, object_list):
+    for obj in object_list:
+        if obj.user == request_user:
+            return True
+    return False
