3444ad99e8ac94780da151e12ea58ff5e8a0f5f6
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-05-14 10:49:31 +0300

    add authorizations and permissions on all Categories operations

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
@@ -11,10 +13,16 @@ class CategoryListView(ListView):
     model = Category
 
 
-class CategoryCreateView(CreateView):
+class CategoryCreateView(UserPassesTestMixin, CreateView):
     model = Category
     form_class = CategoryForm
     success_url = reverse_lazy('dashboard_category_list')
+
+    def test_func(self):
+        condition = self.request.user.is_superuser
+        if not condition:
+            messages.add_message(self.request, messages.ERROR, 'You are not authorized to access this page')
+        return condition
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Add Category'
@@ -25,11 +33,23 @@ class CategoryCreateView(CreateView):
         return super().form_valid(form)
 
 
-class CategoryUpdateView(UpdateView):
+class CategoryUpdateView(UserPassesTestMixin, UpdateView):
     model = Category
     form_class = CategoryForm
     success_url = reverse_lazy('dashboard_category_list')
 
-class CategoryDeleteView(DeleteView):
+    def test_func(self):
+        condition = self.request.user.is_superuser
+        if not condition:
+            messages.add_message(self.request, messages.ERROR, 'You are not authorized to access this page')
+        return condition
+
+class CategoryDeleteView(UserPassesTestMixin,DeleteView):
     model = Category
     success_url = reverse_lazy('dashboard_category_list')
+
+    def test_func(self):
+        condition = self.request.user.is_superuser
+        if not condition:
+            messages.add_message(self.request, messages.ERROR, 'You are not authorized to access this page')
+        return condition
