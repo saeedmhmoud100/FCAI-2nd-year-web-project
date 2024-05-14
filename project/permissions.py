b86a049595852f91088f6fda28a252740492eab3
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-05-14 12:01:55 +0300

    add permissions on wishlist operations

--- project/permissions.py
+++ project/permissions.py
@@ -1,20 +1,24 @@
+from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
+from django.shortcuts import resolve_url
 
 
 def check_user(user):
-    # Replace this with your own test
+
     return user.is_authenticated
 
 
-def check_authenticated(test_func, login_url=None, redirect_field_name=None):
+def check_authenticated(test_func, login_url=None, redirect_field_name=REDIRECT_FIELD_NAME):
     def decorator(view_func):
-        @user_passes_test(test_func, login_url=login_url, redirect_field_name=redirect_field_name)
         def _wrapped_view(request, *args, **kwargs):
             if not test_func(request.user):
-                messages.error(request, 'You need to login to view this page')
+                messages.add_message(request, messages.ERROR, 'You need to login to view this page')
+                path = request.build_absolute_uri()
+                resolved_login_url = resolve_url(login_url or settings.LOGIN_URL)
+                return HttpResponseRedirect('%s?%s=%s' % (resolved_login_url, redirect_field_name, path))
             return view_func(request, *args, **kwargs)
-
         return _wrapped_view
-
     return decorator
--- wishlist/views.py
+++ wishlist/views.py
@@ -2,10 +2,12 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from books.models import Book
+from project.permissions import check_authenticated, check_user
 
 
 # Create your views here.
 
+@check_authenticated(check_user)
 def toggle_book_in_wishlist(request,book_id):
     user = request.user
     book = Book.objects.get(id=book_id)
@@ -15,4 +17,6 @@ def toggle_book_in_wishlist(request,book_id):
         item.delete()
     else:
         messages.add_message(request, messages.INFO, 'This book is added to your wishlist.')
-    return redirect(request.META.get('HTTP_REFERER'))
+        if(request.GET.get('HTTP_REFERER')):
+            return redirect(request.META.get('HTTP_REFERER'))
+    return redirect('book_details',slug=Book.objects.get(id=book_id).slug)
